1bbb1df2497b2f8aec3dd66e512fcdc2e513b4d8
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-04-11 13:40:09 +0200

    Ex 10 completed

--- Es 10 - Blocchi condizionali e cicli/exercise-7.js
+++ Es 10 - Blocchi condizionali e cicli/exercise-7.js
@@ -1,5 +1,9 @@
 function sumUntil(maxValue) {
-  // ...
+  let sommavalori = 0;
+  for(let i=0; i<=maxValue; i++){
+  sommavalori = sommavalori + i
+}
+return sommavalori
 }
 
 console.log(sumUntil(5));
