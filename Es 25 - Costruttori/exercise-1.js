15747fc2393ac7bf4873d8b22c34ac0018fd9554
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-04-27 07:37:31 +0200

    completed ex 25

---
+++ Es 25 - Costruttori/exercise-1.js
@@ -0,0 +1,28 @@
+const person = {
+  get firstName(){
+    return this.nome
+  },
+  set firstName(name){
+    this.nome = name
+  },
+  get lastName(){
+    return this.cognome
+  },
+  set lastName(name){
+    this.cognome = name
+  },
+  fullName(){
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+const john = Object.create(person)
+john.firstName = 'John'
+john.lastName = 'Doe'
+
+const simon = Object.create(person)
+
+simon.firstName = 'Simon'
+simon.lastName = 'Collins'
+
+console.log(john.fullName()); // John Doe
+console.log(simon.fullName()); // Simon Collins
