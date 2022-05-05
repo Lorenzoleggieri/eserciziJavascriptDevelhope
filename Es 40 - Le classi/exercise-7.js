89dfb2f4973c448f78ab32da7c8c2c9176d2c142
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-05-05 16:39:06 +0200

    es 40 completed

--- Es 40 - Le classi/exercise-7.js
+++ Es 40 - Le classi/exercise-7.js
@@ -1,5 +1,39 @@
 class Person {
-  // ...
+
+  constructor(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  set firstName(value) {
+    this._firstName = value;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  set lastName(value) {
+    this._lastName = value;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+    this._age = value;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+
 }
 
 const person = new Person('Mario', 'Rossi', 25);
