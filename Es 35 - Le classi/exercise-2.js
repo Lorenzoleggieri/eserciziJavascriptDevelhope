9074c7c1a4625aee3cdeb52f79982c8486d164fc
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-05-05 11:52:47 +0200

    Es 35 completed

--- Es 35 - Le classi/exercise-2.js
+++ Es 35 - Le classi/exercise-2.js
@@ -1,7 +1,14 @@
 class Person {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName,) {
     this.firstName = firstName;
     this.lastName = lastName;
+  }
+}
+
+class Developer extends Person {
+  constructor (firstName, lastName, role) {
+    super(firstName, lastName);
+    this.role = role
   }
 }
 
