25147e95c6091a53f4519ff41b69cf54babeced7
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-05-05 12:19:00 +0200

    Es 37 completed

--- Es 37 - Le classi/exercise-4.js
+++ Es 37 - Le classi/exercise-4.js
@@ -1,3 +1,19 @@
+class BankAccount {
+constructor (value){
+    this.value = value;
+}
+deposit(depositvalue){
+    this.value += depositvalue
+}
+withdraw(withdrawvalue){
+    this.value -= withdrawvalue;
+}
+view(){
+    console.log(this.value);
+}
+}
+
+
 const bankAccount = new BankAccount(1000);
 bankAccount.deposit(500);
 bankAccount.deposit(200);
