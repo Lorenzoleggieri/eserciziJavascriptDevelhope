e03d9e416e8340d09fd6d1310efd314b61aa9013
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-05-14 19:20:19 +0200

    es 56 completed

--- Es 56 - Rest parameters and spread syntax/exercise-4.js
+++ Es 56 - Rest parameters and spread syntax/exercise-4.js
@@ -1,6 +1,6 @@
-function sum(num1, num2, num3) {
-  return num1 + num2 + num3;
+function sum(...entries) {
+  return entries.reduce((sum, current) => sum + current, 0);
 }
 
 const numbers = [1, 2, 3];
-console.log(sum(numbers[0], numbers[1], numbers[2]));
+console.log(sum(...numbers));
