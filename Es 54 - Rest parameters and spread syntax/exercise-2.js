055dd4d5d90681b87dbdaede6c60aca004d21610
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-05-14 19:13:21 +0200

    es 54 completed

--- Es 54 - Rest parameters and spread syntax/exercise-2.js
+++ Es 54 - Rest parameters and spread syntax/exercise-2.js
@@ -5,11 +5,6 @@ const person = {
   age: 25
 };
 
-const id = person.id;
-const personInfo = {
-  firstName: person.firstName,
-  lastName: person.lastName,
-  age: person.age
-};
+const {id, ...otherStuff} = person
 
-console.log(id, personInfo);
+console.log(id, otherStuff);
